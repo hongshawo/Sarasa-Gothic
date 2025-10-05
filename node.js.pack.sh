ea7c15a1d17306dec512a97f07d990a91258964a
rocky <rocky@ik1-122-67697.vs.sakura.ne.jp>
2025-10-06 02:45:24 +0900

    Commit 251005c

--- node.js.pack.sh
+++ node.js.pack.sh
@@ -2,6 +2,7 @@
 
 l=('CL' 'HC' 'J' 'K' 'SC' 'TC')
 c=7 # 0-7-9
+d='128m'
 v='0.1.0.33.0'
 
 dir=$PWD
@@ -9,15 +10,15 @@ dir=$PWD
 cd out/TTF
 for e in "${l[@]}"
 do
-	7za a -mx=$c "$dir/SarasaStatic$e-TTF-$v.7z" SarasaStatic"$e"-*.ttf
-	7za a -mx=$c "$dir/SarasaStaticSlab$e-TTF-$v.7z" SarasaStaticSlab"$e"-*.ttf
+	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStatic$e-TTF-$v.7z" SarasaStatic"$e"-*.ttf
+	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStaticSlab$e-TTF-$v.7z" SarasaStaticSlab"$e"-*.ttf
 done
 cd "$dir";
 
 cd out/TTF-Unhinted
 for e in "${l[@]}"
 do
-	7za a -mx=$c "$dir/SarasaStatic$e-TTF-Unhinted-$v.7z" SarasaStatic"$e"-*.ttf
-	7za a -mx=$c "$dir/SarasaStaticSlab$e-TTF-Unhinted-$v.7z" SarasaStaticSlab"$e"-*.ttf
+	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStatic$e-TTF-Unhinted-$v.7z" SarasaStatic"$e"-*.ttf
+	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStaticSlab$e-TTF-Unhinted-$v.7z" SarasaStaticSlab"$e"-*.ttf
 done
 cd "$dir";
