8065013736ad6a0ab79abedf7950fefdaab91152
hongshawo <77317622+hongshawo@users.noreply.github.com>
2025-12-05 00:04:30 +0900

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -3,9 +3,9 @@
 l=('CL' 'HC' 'J' 'K' 'SC' 'TC')
 c=7 # 0-7-9
 d='128m'
-v='0.1.0.33.0'
+v='0.1.0.35.0'
 
-if ! 7za | grep -i '^7-Zip.*Igor Pavlov'; then
+if ! 7zz | grep -i '^7-Zip.*Igor Pavlov'; then
 	echo '7-Zip error.'
 	exit
 fi
@@ -15,15 +15,15 @@ dir=$PWD
 cd out/TTF
 for e in "${l[@]}"
 do
-	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStatic$e-TTF-$v.7z" SarasaStatic"$e"-*.ttf
-	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStaticSlab$e-TTF-$v.7z" SarasaStaticSlab"$e"-*.ttf
+	7zz a -mx=$c -md="$d" -bb3 "$dir/SarasaStatic$e-TTF-$v.7z" SarasaStatic"$e"-*.ttf
+	7zz a -mx=$c -md="$d" -bb3 "$dir/SarasaStaticSlab$e-TTF-$v.7z" SarasaStaticSlab"$e"-*.ttf
 done
 cd "$dir";
 
 cd out/TTF-Unhinted
 for e in "${l[@]}"
 do
-	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStatic$e-TTF-Unhinted-$v.7z" SarasaStatic"$e"-*.ttf
-	7za a -mx=$c -md="$d" -bb3 "$dir/SarasaStaticSlab$e-TTF-Unhinted-$v.7z" SarasaStaticSlab"$e"-*.ttf
+	7zz a -mx=$c -md="$d" -bb3 "$dir/SarasaStatic$e-TTF-Unhinted-$v.7z" SarasaStatic"$e"-*.ttf
+	7zz a -mx=$c -md="$d" -bb3 "$dir/SarasaStaticSlab$e-TTF-Unhinted-$v.7z" SarasaStaticSlab"$e"-*.ttf
 done
 cd "$dir";
