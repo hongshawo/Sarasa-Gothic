3be5c479ea841812461be823c9b8a3aaad3f16eb
hongshawo <77317622+hongshawo@users.noreply.github.com>
2026-01-11 01:05:54 +0900

    Update verdafile.mjs

--- verdafile.mjs
+++ verdafile.mjs
@@ -428,7 +428,7 @@ async function MakeProd(t, out, family, region, style, fragT) {
 
 const HintDirPrefix = weight => `${BUILD}/hf-${weight}`;
 // const JHint = oracle("hinting-jobs", async () => os.cpus().length);
-const JHint = oracle("hinting-jobs", async function () { return os.cpus().length + 0; });
+const JHint = oracle("hinting-jobs", async function () { return os.cpus().length - 1; });
 
 const GroupHintStyleList = computed(`group-hint-style-list`, async t => {
 	const [config] = await t.need(Config);
